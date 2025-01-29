9d1a0077fb438f644f8321983a6014621d25e68d
HARSH419 <harshdeveloper369@gmail.com>
2025-01-29 12:08:05 +0530

    format setting & initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    '@react-native-community',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    quotes: ['error', 'single'],
+    'react/no-unescaped-entities': [
+      'error',
+      {
+        forbid: ['>', '"', '}'],
+      },
+    ],
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
+  jsxBracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  // printWidth: 80,
+  printWidth: 800000,
 };
